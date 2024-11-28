b9fa09bef89857190e96f086be5cfa842490dae1
jukr5-eadania <jukr5@student.eadania.dk>
2024-11-28 11:57:29 +0100

    Button collision

--- BlockPusher/Button.cs
+++ BlockPusher/Button.cs
@@ -1,19 +1,57 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 
 namespace BlockPusher
 {
     internal class Button : GameObject
     {
+        Rectangle destinationRectangle;
+        Rectangle source;
+        private bool active;
+        public bool Active { get => active; set => active = value; }
+        public override Rectangle collisionBox
+        {
+            get => destinationRectangle;
+        }
+
+        public Button(Texture2D textureAtlas, Rectangle destinationRectangle, Rectangle source)
+        {
+            this.textureAtlas = textureAtlas;
+            this.destinationRectangle = destinationRectangle;
+            this.source = source;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(textureAtlas, destinationRectangle, source, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0);
+        }
+
         public override void LoadContent(ContentManager content)
         {
-            throw new NotImplementedException();
+            
         }
 
         public override void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            
+        }
+
+        public override void OnCollisionEnter(GameObject other)
+        {
+            if (other is Player || other is Box2)
+            {
+                Active = true;
+            }
+        }
+
+        public override void OnCollisionExit(GameObject other)
+        {
+            if (other is Player || other is Box2)
+            {
+                Active = false;
+            }
         }
     }
 }
--- BlockPusher/Door.cs
+++ BlockPusher/Door.cs
@@ -1,19 +1,43 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 
 namespace BlockPusher
 {
     internal class Door : GameObject
     {
+        Rectangle destinationRectangle;
+        Rectangle source;
+        Button button;
+        public override Rectangle collisionBox
+        {
+            get => destinationRectangle;
+        }
+
+        public Door(Texture2D textureAtlas, Rectangle destinationRectangle, Rectangle source)
+        {
+            this.textureAtlas = textureAtlas;
+            this.destinationRectangle = destinationRectangle;
+            this.source = source;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(textureAtlas, destinationRectangle, source, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0);
+        }
+
         public override void LoadContent(ContentManager content)
         {
-            throw new NotImplementedException();
+            
         }
 
         public override void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            if (button.Active == true)
+            {
+
+            }   
         }
     }
 }
